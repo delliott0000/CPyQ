e8907dffee4a810c30fecb6cc8c22b02c75827c8
Delliott <dlelliott100@outlook.com>
2026-03-08 19:18:42 +0000

    strip back some payload stuff to re-think it

--- Common/WebSocket/payloads.py
+++ Common/WebSocket/payloads.py
@@ -6,25 +6,14 @@ from typing import TYPE_CHECKING
 from ..bases import JSONSerialisableABC
 
 if TYPE_CHECKING:
-    from typing import Any, ClassVar
+    from typing import Any
 
     Json = dict[str, Any]
 
 __all__ = ("Payload", "payload_factory")
 
 
-class Payload(JSONSerialisableABC, ABC):
-    TYPE_MAP: ClassVar[Json | None] = None
-
-    def __init__(self, json: Json, /):
-        if type(self) is Payload:
-            raise NotImplementedError(
-                "Payload is an abstract base class and cannot be instantiated directly."
-            )
-        elif self.TYPE_MAP is None:
-            raise RuntimeError("Payload subclasses must define a type map.")
-
-        ...
+class Payload(JSONSerialisableABC, ABC): ...
 
 
 def payload_factory(json: Json, /) -> Payload: ...
