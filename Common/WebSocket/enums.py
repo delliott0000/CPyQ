cb8d2cc4b8bcaa8aedbe6ee01b211866087e78d2
Delliott <dlelliott100@outlook.com>
2026-03-11 11:03:11 +0000

    change BadPayload to BadPayloadContext

--- Common/WebSocket/enums.py
+++ Common/WebSocket/enums.py
@@ -5,18 +5,18 @@ __all__ = ("CustomWSCloseCode", "CustomWSMessageType", "WSEventStatus")
 
 # fmt: off
 class CustomWSCloseCode(IntEnum):
-    TokenExpired       = 4000
-    InvalidFrameType   = 4001
-    InvalidJSON        = 4002
-    MissingField       = 4003
-    InvalidType        = 4004
-    InvalidValue       = 4005
-    DuplicateEventID   = 4006
-    AckTimeout         = 4007
-    UnknownEvent       = 4008
-    FatalEvent         = 4009
-    BadPayload         = 4010
-    InternalError      = 4999
+    TokenExpired      = 4000
+    InvalidFrameType  = 4001
+    InvalidJSON       = 4002
+    MissingField      = 4003
+    InvalidType       = 4004
+    InvalidValue      = 4005
+    DuplicateEventID  = 4006
+    AckTimeout        = 4007
+    UnknownEvent      = 4008
+    FatalEvent        = 4009
+    BadPayloadContext = 4010
+    InternalError     = 4999
 
 
 class CustomWSMessageType(StrEnum):
