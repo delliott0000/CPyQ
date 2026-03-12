0bfdd81e5f9dcf013d3596b30fd71c5f15f7ef4a
Delliott <dlelliott100@outlook.com>
2026-03-12 19:19:00 +0000

    reset the payload implementation to improve it with codecs

--- Common/WebSocket/payloads.py
+++ Common/WebSocket/payloads.py
@@ -7,53 +7,17 @@ from ..bases import JSONSerialisableABC
 from ..utils import validate
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
-    from typing import Any, ClassVar
+    from typing import Any
 
     Json = dict[str, Any]
-    Validator = Callable[[Any], Any]
 
-__all__ = ("Payload", "EmptyPayload", "payload_factory")
-
-
-class Payload(JSONSerialisableABC, ABC):
-    VALIDATORS: ClassVar[dict[str, Validator] | None] = None
-
-    def __init__(self, json: Json, /):
-        if type(self) is Payload:
-            # Leave this here for safety; ABC will raise TypeError, but don't rely on that
-            raise NotImplementedError(
-                "Payload is an abstract base class and cannot be instantiated directly."
-            )
-        elif self.VALIDATORS is None:
-            raise RuntimeError("Payload subclasses must each define their own validators.")
-
-        # This may be made recursive in the future to handle nested structures
-        for key, validator in self.VALIDATORS.items():
-            setattr(self, key, validator(json[key]))
+__all__ = ("Payload", "payload_factory")
 
 
-class EmptyPayload(Payload):
-    __instance__: EmptyPayload | None = None
-
-    def __new__(cls, _: Json, /):
-        if cls.__instance__ is None:
-            cls.__instance__ = super().__new__(cls)
-        return cls.__instance__
-
-    def __init__(self, _: Json, /):  # noqa
-        pass
-
-    def json(self) -> Json:
-        return {}
+class Payload(JSONSerialisableABC, ABC): ...
 
 
 def payload_factory(json: Json, /) -> Payload:
     validate(dict, json)
 
-    if not json:
-        cls = EmptyPayload
-    else:
-        cls = ...
-
-    return cls(json)
+    return ...
