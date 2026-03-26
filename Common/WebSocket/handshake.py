d1647c98fe158b34a2391801d0cd0fce0c1a1469
Delliott <dlelliott100@outlook.com>
2026-03-26 10:50:59 +0000

    finish implementing HandshakeT and HandshakeManager

--- Common/WebSocket/handshake.py
+++ Common/WebSocket/handshake.py
@@ -5,12 +5,15 @@ from typing import TYPE_CHECKING, Generic, TypeVar
 
 from .payloads import Handshake
 
-HandshakeT = TypeVar("HandshakeT", bound=Handshake)
-
 if TYPE_CHECKING:
     from asyncio import Future
 
-__all__ = ("HandshakeManager",)
+    from ..errors import WSException
+
+__all__ = ("HandshakeT", "HandshakeManager")
+
+
+HandshakeT = TypeVar("HandshakeT", bound=Handshake)
 
 
 class HandshakeManager(Generic[HandshakeT]):
@@ -30,7 +33,56 @@ class HandshakeManager(Generic[HandshakeT]):
         expect_wired: bool | None = None,
         expect_done: bool | None = None,
         expect_fail: bool | None = None,
-    ) -> None: ...
+    ) -> None:
+        pairs = (
+            ("Handshake", expect_handshake, self.has_handshake),
+            ("Wired", expect_wired, self.is_wired),
+            ("Done", expect_done, self.is_done),
+            ("Fail", expect_fail, self.is_fail),
+        )
+        for name, expected, actual in pairs:
+            if expected is not None and expected != actual:
+                raise RuntimeError(f"{name} state mismatch; expected {expected}, got {actual}.")
+
+    @property
+    def has_handshake(self) -> bool:
+        return self.__handshake is not None
+
+    def set_handshake(self, handshake: HandshakeT, /) -> None:
+        if not isinstance(handshake, self.__cls):
+            raise TypeError(f"An instance of {self.__cls.__name__} is required.")
+
+        self.__pre_check__(expect_handshake=False, expect_fail=False)
+        self.__handshake = handshake
+
+    @property
+    def is_wired(self) -> bool:
+        return self.__wired
+
+    def set_wired(self) -> None:
+        self.__pre_check__(expect_handshake=True, expect_wired=False, expect_fail=False)
+        self.__wired = True
+
+    @property
+    def is_done(self) -> bool:
+        return self.__done_futr.done() and self.__done_futr.exception() is None
+
+    def set_done(self) -> None:
+        self.__pre_check__(expect_wired=True, expect_done=False, expect_fail=False)
+        self.__done_futr.set_result(self.__handshake)
+
+    @property
+    def is_fail(self) -> bool:
+        return self.__done_futr.done() and self.__done_futr.exception() is not None
+
+    def set_fail(self, error: WSException, /) -> None:
+        self.__pre_check__(expect_done=False, expect_fail=False)
+        self.__done_futr.set_exception(error)
+
+    @property
+    def handshake(self) -> HandshakeT:
+        self.__pre_check__(expect_handshake=True, expect_fail=False)
+        return self.__handshake
 
     async def wait_for_done(self) -> HandshakeT:
         return await self.__done_futr
