06da7aaacd6d22828abd724d72ad82f4ce535001
Delliott <dlelliott100@outlook.com>
2026-04-08 12:59:36 +0100

    move send_handshake to CustomWSResponse

--- Common/WebSocket/response.py
+++ Common/WebSocket/response.py
@@ -210,10 +210,6 @@ class WSResponseMixin(Generic[HandshakeT]):
         event = WSEvent.from_payload(payload, **kwargs)
         await self.__send_event__(event)
 
-    async def send_handshake(self) -> None:
-        self.__handshake_manager.set_wired()
-        await self.send_payload(self.__handshake_manager.handshake)
-
     async def close(self, _cancel_all: bool = True, **kwargs: Any) -> bool:
         result = await super().close(**kwargs)  # noqa
 
@@ -236,6 +232,10 @@ class WSResponseMixin(Generic[HandshakeT]):
 
 class CustomWSResponse(WSResponseMixin[HandshakeT], WebSocketResponse, Generic[HandshakeT]):
     SERVER = True
+
+    async def send_handshake(self) -> None:
+        self.__handshake_manager.set_wired()
+        await self.send_payload(self.__handshake_manager.handshake)
 
 
 class CustomClientWSResponse(
