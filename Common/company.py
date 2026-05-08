c70cbe8cda6e67916b9c9786aabef369a9dc0d86
Delliott <dlelliott100@outlook.com>
2026-05-08 15:23:29 +0100

    re-work Company to IntIdentifiable subclass

--- Common/company.py
+++ Common/company.py
@@ -1,39 +1,15 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-from .bases import ComparesIDABC, ComparesIDMixin, JSONSerialisableABC
-
-if TYPE_CHECKING:
-    from typing import Any
-
-    from asyncpg import Record
-
-    Json = dict[str, Any]
+from .bases_new import IntIdentifiable
+from .codecs import PrimitiveCodec
 
 __all__ = ("Company",)
 
 
-class Company(ComparesIDMixin, ComparesIDABC, JSONSerialisableABC):
-    __slots__ = ("_id", "_name")
+class Company(IntIdentifiable):
+    codecs = {
+        "name": PrimitiveCodec(str),
+    }
 
-    def __init__(self, company_record: Record | Json, /):
-        self._id = company_record["id"]
-        self._name = company_record["name"]
+    name: str
 
     def __str__(self):
-        return self._name
-
-    @property
-    def id(self) -> int:
-        return self._id
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    def json(self) -> Json:
-        return {
-            "id": self._id,
-            "name": self._name,
-        }
+        return self.name
