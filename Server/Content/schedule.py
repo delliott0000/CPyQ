abdd22162c6c32dcaf70f158a4319501bbfb32c1
Delliott <dlelliott100@outlook.com>
2026-05-29 11:34:33 +0100

    begin to re-do the AutopilotManager contract

--- Server/Content/schedule.py
+++ Server/Content/schedule.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    ...
+    from Common import Token
 
 __all__ = ("Autopilot", "AutopilotManager")
 
@@ -11,4 +11,9 @@ __all__ = ("Autopilot", "AutopilotManager")
 class Autopilot: ...
 
 
-class AutopilotManager: ...
+class AutopilotManager:
+    def __init__(self): ...
+
+    async def autopilot_connect(self, token: Token, /) -> Autopilot: ...
+
+    async def autopilot_disconnect(self, token: Token, /) -> Autopilot: ...
