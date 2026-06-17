0f7b617b860550b4a0246695606ffe47999bf881
Delliott <dlelliott100@outlook.com>
2026-06-17 09:04:26 +0100

    finish moving stuff to tools.py

--- Common/Tasks/task.py
+++ Common/Tasks/task.py
@@ -8,17 +8,8 @@ from .enums import TaskSort
 
 if TYPE_CHECKING:
     from datetime import datetime
-    from typing import Any
 
-    Json = dict[str, Any]
-
-__all__ = (
-    "Task",
-    "task_sort_to_cls",
-    "task_cls_to_sort",
-    "parse_received_task",
-    "build_task",
-)
+__all__ = ("Task",)
 
 
 class Task(IntIdentifiable):
@@ -35,38 +26,3 @@ class Task(IntIdentifiable):
     @property
     def pending(self) -> bool:
         return self.completed_at is None
-
-
-_TASK_MAP: dict[TaskSort, type[Task]] = {}
-
-
-def task_sort_to_cls(sort: TaskSort, /) -> type[Task]:
-    return _TASK_MAP[sort]
-
-
-_TASK_RMAP: dict[type[Task], TaskSort] = {v: k for k, v in _TASK_MAP.items()}
-
-
-def task_cls_to_sort(cls: type[Task], /) -> TaskSort:
-    return _TASK_RMAP[cls]
-
-
-def parse_received_task(json: Json, /) -> Task:
-    sort = TaskSort(json["sort"])
-    cls = task_sort_to_cls(sort)
-    return cls(json)
-
-
-if TYPE_CHECKING:
-    from typing import TypeVar
-
-    TaskT = TypeVar("TaskT", bound=Task)
-
-
-def build_task(cls: type[TaskT], json: Json, /) -> TaskT:
-    if "sort" in json:
-        raise ValueError('The supplied JSON contains a "sort" field.')
-
-    sort = task_cls_to_sort(cls)
-    real_json = {**json, "sort": sort.value}
-    return cls(real_json)
--- Common/Tasks/tools.py
+++ Common/Tasks/tools.py
@@ -2,16 +2,24 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from .enums import TaskSort
+
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from typing import TypeVar
+    from typing import Any, TypeVar
 
-    from .enums import TaskSort
     from .task import Task
 
+    Json = dict[str, Any]
     TaskT = TypeVar("TaskT", bound=Task)
 
-__all__ = ("register_sort", "task_sort_to_cls", "task_cls_to_sort")
+__all__ = (
+    "register_sort",
+    "task_sort_to_cls",
+    "task_cls_to_sort",
+    "parse_received_task",
+    "build_task",
+)
 
 
 _TASK_MAP: dict[TaskSort, type[Task]] = {}
@@ -35,3 +43,18 @@ def task_sort_to_cls(sort: TaskSort, /) -> type[Task]:
 
 def task_cls_to_sort(cls: type[Task], /) -> TaskSort:
     return _TASK_RMAP[cls]
+
+
+def parse_received_task(json: Json, /) -> Task:
+    sort = TaskSort(json["sort"])
+    cls = task_sort_to_cls(sort)
+    return cls(json)
+
+
+def build_task(cls: type[TaskT], json: Json, /) -> TaskT:
+    if "sort" in json:
+        raise ValueError('The supplied JSON contains a "sort" field.')
+
+    sort = task_cls_to_sort(cls)
+    real_json = {**json, "sort": sort.value}
+    return cls(real_json)
