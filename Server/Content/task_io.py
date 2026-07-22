e9b764d024192bb0db59c1c6982312038d8cae63
Delliott <dlelliott100@outlook.com>
2026-07-22 14:11:02 +0100

    define task_reader and get_task_reader

--- Server/Content/task_io.py
+++ Server/Content/task_io.py
@@ -3,6 +3,33 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    ...
+    from collections.abc import Callable, Coroutine
+    from typing import Any
 
-__all__ = ()
+    from Common import TaskSort
+
+    from .postgresql import PostgreSQLClient
+
+    Json = dict[str, Any]
+    ReaderCoro = Coroutine[Any, Any, Json]
+    ReaderFunc = Callable[[PostgreSQLClient, Json], ReaderCoro]
+    ReaderDeco = Callable[[ReaderFunc], ReaderFunc]
+
+__all__ = ("task_reader", "get_task_reader")
+
+
+_TASK_READERS: dict[TaskSort, ReaderFunc] = {}
+
+
+def task_reader(sort: TaskSort, /) -> ReaderDeco:
+
+    def wrapper(func: ReaderFunc, /) -> ReaderFunc:
+        _TASK_READERS[sort] = func
+
+        return func
+
+    return wrapper
+
+
+def get_task_reader(sort: TaskSort, /) -> ReaderFunc:
+    return _TASK_READERS[sort]
