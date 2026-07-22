54e933097fb323596955ff00ea618272f90115c3
Delliott <dlelliott100@outlook.com>
2026-07-22 14:15:22 +0100

    add our first real value to task_sort sql enum

--- SQL/Schema/Types/task_sort_enum.sql
+++ SQL/Schema/Types/task_sort_enum.sql
@@ -2,8 +2,7 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'task_sort') THEN
         CREATE TYPE task_sort AS ENUM (
-        -- Remove the placeholder value when real values are ready to be implemented
-        '__placeholder__'
+        'export_quote'
     );
     END IF;
 END
